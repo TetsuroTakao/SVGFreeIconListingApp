9213d3a39f988b0667bd2380e671ba3abad202f8
Tetsuro Takao <tetsuro.takao@processtune.com>
2021-12-16 21:23:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -18,10 +18,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// APIs
+app.get('/freeIcons', (req) => {
+  var account = req.query.account;
+  if(account == null){
 
+  }
+  else{
+    
+  }
+}, (req,res) => {
+
+  res.json([{icon : 'icon1 of freeIcons'},{icon : 'icon2 of freeIcons'}]);
+});
+app.post('/setIcon',(req,res) => {
+  res.writeHead(200,{'Content-Type' : 'application/json'});
+  res.end(JSON.stringify({message : 'setIcon called'}));
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
